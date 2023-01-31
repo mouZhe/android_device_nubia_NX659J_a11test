79acf8fe2647a8e59405ad906d933a8d8c4a3fdc
某哲 <50080113+mouZhe@users.noreply.github.com>
2023-01-31 14:04:41 +0800

    Update omni_NX659J.mk

--- omni_NX659J.mk
+++ omni_NX659J.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/nubia/NX659J
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
